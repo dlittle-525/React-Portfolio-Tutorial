6b434a3942f5e03567ea4a2d49fb9585f56cf6d4
Douglas Little <douglas.a.little@gmail.com>
2020-09-11 19:51:47 -0400

    Home page done

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div className='title'>
+            <h1></h1>
+            
+        </div>
+    );
+};
+
+export default Hero;
---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div className="container-nav">
+        <header>
+            <h1>Doug Little's Portfolio</h1>
+            <nav>
+                <a href="#" className="nav-link">Home    </a>
+                <a href="#" className="nav-link">About Me    </a>
+                <a href="#" className="nav-link">Projects</a>
+            </nav>
+          </header>          
+        </div>
+    )
+}
+
+export default Nav;
